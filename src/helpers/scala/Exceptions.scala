b19a06b0d3a5c2a17a7d14516df523bec3301705
Gidon Ernst <ernst@isse.de>
2015-12-15 14:21:16 +0100

    add license

--- src/helpers/scala/Exceptions.scala
+++ src/helpers/scala/Exceptions.scala
@@ -1,3 +1,7 @@
+// Flashix: a verified file system for flash memory
+// (c) 2015 Institute for Software & Systems Engineering <http://isse.de/flashix>
+// This code is licensed under MIT license (see LICENSE for details)
+
 package helpers.scala
 
 case class NumberException(msg : String) extends Exception("number exception: " + msg) { }
--- src/types/types.scala
+++ src/types/types.scala
@@ -1,3 +1,7 @@
+// Flashix: a verified file system for flash memory
+// (c) 2015 Institute for Software & Systems Engineering <http://isse.de/flashix>
+// This code is licensed under MIT license (see LICENSE for details)
+
 import helpers.scala._
 
 package object types {
